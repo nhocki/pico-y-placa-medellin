794d184b3a69301fdfb62f26a215ba97419c65e1
Nicolás Hock Isaza <nhocki@gmail.com>
2017-01-17 10:48:54 -0500

    🚗 and 🏍

--- js/app.js
+++ js/app.js
@@ -19,17 +19,17 @@ var carPlates = [
 ];
 
 var bikePlates = [
-  "No hay pico y placa",
+  "",
   "4 - 5",
   "6 - 7",
   "8 - 9",
   "0 - 1",
   "2 - 3",
-  "No hay pico y placa"
+  ""
 ];
 
 
 var date = new Date();
 document.getElementById("day").innerHTML = days[ date.getDay() ];
-document.getElementById("car-plates").innerHTML = carPlates[ date.getDay() ];
-document.getElementById("bike-plates").innerHTML = bikePlates[ date.getDay() ];
+document.getElementById("car-plates").innerHTML = "🚗  " + carPlates[ date.getDay() ];
+document.getElementById("bike-plates").innerHTML = "🏍  " + bikePlates[ date.getDay() ];
