f7aa663aefe6e45661516ec0129f53b55da89dc4
Nicolás Hock Isaza <nhocki@gmail.com>
2017-03-22 16:50:53 -0500

    Alerta Roja!

--- js/app.js
+++ js/app.js
@@ -3,9 +3,9 @@ var days = [
   "Lunes",
   "Martes",
   "Miércoles",
-  "Jueves",
-  "Viernes",
-  "Sábado"
+  "Jueves ❗️",
+  "Viernes ❗️",
+  "Sábado ❗️"
 ];
 
 var carPlates = [
@@ -28,8 +28,28 @@ var bikePlates = [
   ""
 ];
 
+var redAlertCars = [
+  "",
+  "",
+  "",
+  "0 - 1 - 2 - 3",
+  "4 - 5 - 6 - 7 - 8 - 9",
+  "8 - 9 - 0 - 1 - 2 - 3",
+  "2 - 3 - 4 - 5 - 6 - 7"
+]
+
+var redAlertBikes = [
+  "",
+  "",
+  "",
+  "6 - 7",
+  "8 - 9 - 0 - 1 - 2 - 3",
+  "0 - 1 - 2 - 3 - 4 - 5",
+  "4 - 5 - 6 - 7 - 8 - 9"
+]
+
 
 var date = new Date();
-document.getElementById("day").innerHTML = days[ date.getDay() ];
-document.getElementById("car-plates").innerHTML = "🚗  " + carPlates[ date.getDay() ];
-document.getElementById("bike-plates").innerHTML = "🏍  " + bikePlates[ date.getDay() ];
+document.getElementById("day").innerHTML = days[ date.getDay()];
+document.getElementById("car-plates").innerHTML = "🚗  " + redAlertCars[ date.getDay()];
+document.getElementById("bike-plates").innerHTML = "🏍  " + redAlertBikes[ date.getDay()];
